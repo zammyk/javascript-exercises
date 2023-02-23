fbfd2c6e6f27c923e3d864b5ff8c88f779393680
Rahul Hembrom <69256240+zammyk@users.noreply.github.com>
2023-02-23 21:40:46 +0530

    complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if(times<0)
+        return "ERROR";
+    let finalString = "";
+    while(times--){
+        finalString += string;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
