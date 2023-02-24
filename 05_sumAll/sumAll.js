a04fa74a4ac24d8dcbbb7abfe5f3cbcc5a36bd51
Rahul Hembrom <69256240+zammyk@users.noreply.github.com>
2023-02-24 20:04:57 +0530

    complete task 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(lowerLimit, upperLimit) {
+    if(typeof(lowerLimit)!="number"||typeof(upperLimit)!="number"|| Math.min(lowerLimit,upperLimit)<0)
+        return "ERROR";
+    if(lowerLimit>upperLimit)
+        [lowerLimit,upperLimit]=[upperLimit,lowerLimit];
+    let sum =0;
+    for(let num = lowerLimit;num<=upperLimit;num++)
+        sum+=num;
+    return sum;
 };
 
 // Do not edit below this line
