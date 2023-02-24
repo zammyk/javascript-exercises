cf86bbd2cf3448d8a31c2afe88fac9fec63bc7b4
Rahul Hembrom <69256240+zammyk@users.noreply.github.com>
2023-02-24 19:56:43 +0530

    complete task 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    for(const toRemove of args){
+        for(let i = 0;i<arr.length;i++){
+            if(arr[i] === toRemove)
+            {
+                arr.splice(i,1);
+                i--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
